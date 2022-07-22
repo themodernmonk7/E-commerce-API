445f7277903654561cbd06f72f68bc39e381352e
themodernmonk7 <sauravavishek@gmail.com>
2022-07-22 21:44:17 +0530

    Added the Register user functionalities in authController file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,23 @@
 const User = require("../models/userModel")
+const { StatusCodes } = require("http-status-codes")
+const CustomError = require("../errors")
+const { createTokenUser, attachCookiesToResponse } = require("../utils")
 
 // Register User
 const register = async (req, res) => {
-  res.send("Register User")
+  const { email, name, password } = req.body
+  const emailAlreadyExists = await User.findOne({ email })
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists")
+  }
+  // Add first registered user as admin
+  const isFirstAccount = (await User.countDocuments({})) === 0
+  const role = isFirstAccount ? "admin" : "user"
+  const user = await User.create({ email, name, password, role })
+  // Create token user
+  const tokenUser = createTokenUser(user)
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 
 // Login User
