493449b3744d940ff7b0405c372d3c9214adac75
themodernmonk7 <sauravavishek@gmail.com>
2022-07-31 20:16:42 +0530

    Show current user complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,14 @@ const { StatusCodes } = require("http-status-codes")
 const CustomError = require("../errors")
 const { createTokenUser, attachCookiesToResponse } = require("../utils")
 
-// Get all users
+//** ======================== Get all users ========================
 const getAllUsers = async (req, res) => {
-  // console.log(req.user) //check if I am getting user or not
+  // console.log(req.user) //check if I am getting req.user from authentication.js or not
   const user = await User.find({ role: "user" }).select("-password")
   res.status(StatusCodes.OK).json({ total_users: user.length, user })
 }
 
-// Get single user
+//** ======================== Get single user ========================
 const getSingleUser = async (req, res) => {
   const { id: userId } = req.params
   const user = await User.findOne({ _id: userId }).select("-password")
@@ -20,17 +20,17 @@ const getSingleUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user })
 }
 
-// Show current user
+//** ======================== Show current user ========================
 const showCurrentUser = async (req, res) => {
-  res.send("Show current user")
+  res.status(StatusCodes.OK).json({user: req.user})
 }
 
-// Update user
+//** ======================== Update user ========================
 const updateUser = (req, res) => {
   res.send("Update user")
 }
 
-// Updat user password
+//** ======================== Update user password ========================
 const updateUserPassword = async (req, res) => {
   res.send("Update user password")
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,7 @@ const {
 router
   .route("/")
   .get(authenticateUser, authorizePermissions("admin", "owner"), getAllUsers)
-router.route("/showMe").get(showCurrentUser)
+router.route("/showMe").get(authenticateUser, showCurrentUser)
 router.route("/updateUser").patch(updateUser)
 router.route("/updateUserPassword").patch(updateUserPassword)
 router.route("/:id").get(authenticateUser, getSingleUser)
