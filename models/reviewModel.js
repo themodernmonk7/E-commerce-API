87c36a6d94b75c03d68cdfa1767343aac65143be
themodernmonk7 <sauravavishek@gmail.com>
2022-08-05 15:40:52 +0530

    Feat: Average rating of a product

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -39,5 +39,40 @@ const ReviewSchema = new mongoose.Schema(
 // User can leave only one review for a product
 ReviewSchema.index({product: 1, user: 1}, {unique: true})
 
+// Average rating
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+    //console.log(result) // {_id: null, averageRating: 4, numOfReviews: 2}
+  ])
+
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        // 👇 This is optional chaining in JavaScript
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product)
+})
+ReviewSchema.post("remove", async function () {
+  await this.constructor.calculateAverageRating(this.product)
+})
+
 
 module.exports = mongoose.model("Review", ReviewSchema)
